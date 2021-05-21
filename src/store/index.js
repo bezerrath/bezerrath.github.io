dff0eaf812d1028fea011506802d3ac20be29ba4
Thiago Bezerra <bezerrath@gmail.com>
2021-05-21 01:00:20 -0300

    Set store scaffolding

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import pokemons from '@/store/modules/pokemons'
 
 export default createStore({
   state: {
@@ -8,5 +9,6 @@ export default createStore({
   actions: {
   },
   modules: {
+    pokemons
   }
 })
