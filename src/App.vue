507154b302b1c464ba1de8ff378189c1e48a0e6d
Thiago Bezerra <bezerrath@gmail.com>
2021-05-23 17:43:33 -0300

    Create global styles

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,97 @@
 <template>
-  <TheHeader />
   <router-view />
 </template>
 <script>
-import TheHeader from '@/components/TheHeader'
-export default {
-  components: {
-    TheHeader
-  }
-}
+export default { }
 </script>
 
 <style lang="scss">
+body{
+  padding: 0px;
+  margin: 0px;
+  background-color: #121212;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #bfbfbf
 }
+
+.header {
+  display: flex;
+  flex-wrap: wrap;
+  background: #272727;
+  align-items: center;
+  color: #bfbfbf;
+  height: 64px;
+  border: 1px solid #1e1e1e;
+  border-bottom-color: #414141;
+}
+
+.header img {
+  margin: 0px 10px 0px 10px;
+  margin-top: -10px;
+}
+
+.flex {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 5px;
+  flex: 1 1 240px;
+  margin: 5px;
+}
+
+.flex > div {
+  flex: 1 1 240px;
+  margin: 5px;
+}
+
+.grid {
+  display: grid;
+}
+
+.grid > div {
+  grid-template-columns: 50%;
+  grid-auto-rows: 100px;
+}
+
+.image {
+  width: 100%;
+  max-width: 300px;
+  height: auto;
+  display: block;
+}
+
+.clickable {
+  cursor: pointer;
+}
+
+.input-search {
+  border-radius: 10px;
+  color: #bfbfbf;
+  background-color: #1e1e1e;
+  border: none;
+  height: 46px;
+  font-size: 16px;
+  outline: none;
+}
+
+@media (max-width: 600px){
+  .flex-carousel{
+    flex-wrap: nowrap;
+    overflow-x: auto;
+    scroll-snap-type: x mandatory;
+    -webkit-overflow-scrolling: touch;
+    scroll-behavior: smooth;
+  }
+  .flex-carousel > div {
+    flex: 1 0 240px;
+    scroll-snap-align: start;
+  }
+  .image {
+    max-width: 100%;
+  }
+}
+
 </style>
