ccb00756e8a4255aa1ade586872089cece9a3558
Thiago Bezerra <bezerrath@gmail.com>
2021-05-23 17:42:54 -0300

    Create Navbar component

---
+++ src/components/BaseNavbar.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="header">
+        <router-link to="/"><img src="/img/icons/android-icon-48x48.png" alt="Pokemon Vue"></router-link>
+        <slot name="actions"></slot>
+    </div>
+</template>
